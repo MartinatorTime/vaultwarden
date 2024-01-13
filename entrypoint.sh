24a76c78daa3d7cadd81a3b4cd873ce0d5f80219
Your Name <your.email@example.com>
2024-01-13 09:33:35 +0200

    (workflows): update workflow files
    - Removed unnecessary repositories from check-releases workflow
    - Renamed clear-old-releases workflow to Delete Older that 7Days Release Files
    - Removed scheduled trigger from clear-old-releases workflow
    - Removed scheduled trigger from delete-runs workflow
    - Updated deploy-fly-dev and deploy-fly workflows to modify flyctl deploy command options
    - Updated Dockerfile.fly entrypoint script for better performance

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
-# Increase the ping_group_range
-echo "0 1024" > /proc/sys/net/ipv4/ping_group_range
-
+#
 # Set sysctl parameters
 sysctl -w net.core.rmem_max=8388608
 sysctl -w net.core.wmem_max=8388608
@@ -10,6 +7,7 @@ sysctl -w net.ipv4.tcp_rmem='4096 87380 16777216'
 sysctl -w net.ipv4.tcp_wmem='4096 87380 16777216'
 sysctl -w net.ipv4.tcp_mem='16777216 16777216 16777216'
 sysctl -w net.ipv4.udp_mem='16777216 16777216 16777216'
-
+sysctl -w net.ipv4.ping_group_range="0 1000"
+#
 # Execute the CMD
 exec "$@"
