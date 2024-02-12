04c5cab58f3d731f3017b43cdd0dc7c1047345f4
MartinatorTime <76610490+MartinatorTime@users.noreply.github.com>
2024-02-12 16:28:51 +0200

    🛠️ .github/workflows/deploy-fly-dev.yml -> Updated setup-task to v2, added new secrets for B2 application, and modified Checkout Repository step. 🛠️ .github/workflows/deploy-fly.yml -> Updated setup-task to v2, added new secrets for B2 application, and modified Checkout Repository step. 🛠️ Dockerfile.fly -> Removed Backblaze B2 CLI installation, updated B2 download link, and renamed backup script. 🛠️ config/crontab -> Updated cron jobs to use new backup scripts. 🛠️ entrypoint.sh -> Removed unnecessary comments and whitespace. 🛠️ scripts/backup-r2.sh -> Updated B2 authorization and file upload process.

--- scripts/backup-r2.sh
+++ scripts/backup-r2.sh
@@ -2,22 +2,17 @@
 
 sqlite3 /data/db.sqlite3 '.backup /data/db.bak'
 tar -czf /backup.tar.gz /data
-
+#
 # Encrypt File and Upload to Backblaze B2
 echo "$PASS" | gpg --batch --yes --passphrase-fd  0 --cipher-algo AES256 --symmetric backup.tar.gz
-
-# Set B2 account info
-B2_ACCOUNT_ID="$B2_KEY_ID"
-B2_APPLICATION_KEY="$B2_APP_KEY"
-B2_BUCKET_NAME="$B2_BUCKET"
-
+#
 # Authorize with Backblaze B2
-b2 authorize-account $B2_ACCOUNT_ID $B2_APPLICATION_KEY
-
+b2 authorize-account "$B2_APPLICATION_KEY_ID" "$B2_APPLICATION_KEY"
+#
 # Upload the encrypted backup file to B2
-FILE_NAME="backup-$(date +'%d_%m_%Y-%H_%M').tar.gz.gpg"
-b2 upload-file $B2_BUCKET_NAME $FILE_NAME /path/to/backup.tar.gz.gpg
-
+FILE_NAME="DATA-backup-$(date +'%d_%m_%Y-%H_%M').tar.gz.gpg"
+b2 upload-file $B2_BUCKET backup.tar.gz.gpg $FILE_NAME
+#
 # Remove local files
 rm *.tar.gz.gpg
 rm *.tar.gz
