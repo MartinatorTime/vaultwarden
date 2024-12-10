3b6dffd73734db347a596221c93d2f5c3735f807
MartinatorTime <76610490+MartinatorTime@users.noreply.github.com>
2024-12-10 15:40:58 +0200

    Update start_cloudflared.sh

--- scripts/start_cloudflared.sh
+++ scripts/start_cloudflared.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cloudflared tunnel --no-autoupdate run --protocol quic --token "$CF_TOKEN" --url "http://localhost:8080"
+cloudflared tunnel --no-autoupdate run --protocol http2 --token "$CF_TOKEN" --url "http://localhost:8080"
