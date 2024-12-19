a3783f4b782d25809d8fb711416987e0151db81a
MartinatorTime <email@email.com>
2024-12-19 21:02:00 +0200

    Test with skip sync if vaultwarden.log is written

--- scripts/sync-r2-rclone.sh
+++ scripts/sync-r2-rclone.sh
@@ -28,6 +28,18 @@ chmod 600 /root/.config/rclone/rclone.conf
 REMOTE_NAME="Cloudflare"
 REMOTE_PATH="vaultwarden-data/data"
 
+is_file_being_written() {
+  local file="$1"
+  local size1=$(stat -c %s "$file")
+  sleep 5
+  local size2=$(stat -c %s "$file")
+  if [ "$size1" -ne "$size2" ]; then
+    return 0 # File is being written
+  else
+    return 1 # File is not being written
+  fi
+}
+
 LAST_MODIFIED=$(find /data -type f -exec stat -c %Y {} \; | sort -n | tail -1)
 
 while true; do
@@ -36,21 +48,19 @@ while true; do
 
   # Check if the /data directory has been modified since the last sync
   if [ $CURRENT_MODIFIED -gt $LAST_MODIFIED ]; then
-    # Check if vaultwarden.log is in use
-    while lsof ./data/vaultwarden.log > /dev/null 2>&1; do
+    if is_file_being_written "/data/vaultwarden.log"; then
       if [ "$R2_DATA_SYNC_LOG" = "true" ]; then
-        echo "vaultwarden.log is in use, waiting..."
+        echo "Sync skipped, vaultwarden.log is being written to."
       fi
-      sleep 5
-    done
-
-    if [ "$R2_DATA_SYNC_LOG" = "true" ]; then
-      rclone sync ./data $REMOTE_NAME:$REMOTE_PATH
-      echo "Sync completed successfully!"
     else
-      rclone sync ./data $REMOTE_NAME:$REMOTE_PATH
+      if [ "$R2_DATA_SYNC_LOG" = "true" ]; then
+        rclone sync ./data $REMOTE_NAME:$REMOTE_PATH
+        echo "Sync completed successfully!"
+      else
+        rclone sync ./data $REMOTE_NAME:$REMOTE_PATH
+      fi
+      LAST_MODIFIED=$CURRENT_MODIFIED
     fi
-    LAST_MODIFIED=$CURRENT_MODIFIED
   else
     if [ "$R2_DATA_SYNC_LOG" = "true" ]; then
       echo "Sync skipped, no changes detected."
