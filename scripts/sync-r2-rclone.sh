ba0d2b4e8b94ae94415d6a9da84a7fc614fb05bf
MartinatorTime <email@email.com>
2024-12-19 22:11:16 +0200

    Test

--- scripts/sync-r2-rclone.sh
+++ scripts/sync-r2-rclone.sh
@@ -37,10 +37,10 @@ while true; do
   # Check if the /data directory has been modified since the last sync
   if [ $CURRENT_MODIFIED -gt $LAST_MODIFIED ]; then
     if [ "$R2_DATA_SYNC_LOG" = "true" ]; then
-      rclone sync ./data $REMOTE_NAME:$REMOTE_PATH --delete-during
+      rclone sync ./data $REMOTE_NAME:$REMOTE_PATH --ignore-errors
       echo "Sync completed successfully!"
     else
-      rclone sync ./data $REMOTE_NAME:$REMOTE_PATH --delete-during
+      rclone sync ./data $REMOTE_NAME:$REMOTE_PATH --ignore-errors
     fi
     LAST_MODIFIED=$CURRENT_MODIFIED
   else
