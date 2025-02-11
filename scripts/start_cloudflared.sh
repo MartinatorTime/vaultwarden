65b03dbcae549f408fe245094d1c3a8ca38501b6
Username <user@user@user.com>
2025-02-11 15:07:02 +0200

    update start_cloudflared.sh

--- scripts/start_cloudflared.sh
+++ scripts/start_cloudflared.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cloudflared tunnel --no-autoupdate run --protocol quic --token "$CF_TOKEN" --loglevel "debug"
+cloudflared tunnel --no-autoupdate run --protocol quic --token "$CF_TOKEN"
