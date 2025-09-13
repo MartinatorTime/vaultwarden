0763f64796fd4477d6c8cb6023b9925f1cf7fae0
MartinatorTime <martinatortime@gmail.com>
2025-09-13 18:46:29 +0300

    update start_cloudflared.sh

--- scripts/start_cloudflared.sh
+++ scripts/start_cloudflared.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cloudflared tunnel --autoupdate-freq "24h" --protocol "quic" --loglevel "error" --url "http://localhost:80" --token "$CF_TOKEN"
+cloudflared tunnel --autoupdate-freq 24h --protocol quic --url "http://localhost:80" --token "$CF_TOKEN"
